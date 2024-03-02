9e20bf8a4dd509c704d0ba7203b5dcc38bd5423d
Wahyu Eko <wahyu.ehs@gmail.com>
2024-03-02 22:09:08 +0700

    init

---
+++ src/main/java/com/example/demo/HaiController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HaiController {
+    @GetMapping("/hello")
+    public String extraController() {
+        return "ok";
+    }
+}
